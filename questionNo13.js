11a6ddcfc87c916b03da912bcf3fc6d51a4c699d
gangahasan <rgangabhavani2@gmail.com>
2024-12-06 10:15:44 +0530

    reduce method implemented

---
+++ questionNo13.js
@@ -0,0 +1,23 @@
+let products = [
+  "electronics",
+  "clothing",
+  "electronics",
+  "toys",
+  "clothing",
+  "toys",
+  "toys",
+];
+
+let countObject = products.reduce((a, item) => {
+  if (products.includes(item)) {
+    let count = a[item] ?? 0;
+    count++;
+    a[item] = count;
+    return a;
+  }
+}, {});
+
+console.log(countObject);
+
+let sortObject = Object.entries(countObject).sort((a,b)=>b[1]-a[1]);
+console.log(sortObject);
